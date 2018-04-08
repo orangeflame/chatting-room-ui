cc6343263e36a9a064696f1a2cf6c914cbb94bdf
le.zheng <le.zheng@ef.com>
2018-04-08 14:22:40 +0800

    make left bubble works as its name describes

--- modules/chat/components/Bubble/BubbleFromLeft/styled.ts
+++ modules/chat/components/Bubble/BubbleFromLeft/styled.ts
@@ -4,17 +4,20 @@ import { Component } from "./component";
 
 export const Styled = styled(Component)`
   display: flex;
-  margin: ${({ theme }) => theme.sizeIn(-10)};
+  flex-direction: row-reverse;
+  justify-content: flex-end;
+  margin: ${(props) => props.theme.sizeIn(-10)};
   > div {
-    background: ${({ theme }) => theme.colorPrimary};
+    background: ${({ theme }) => theme.colorBg};
     border-radius: ${({ theme }) => theme.sizeCornerBubble};
-    border-top-right-radius: 0;
+    border-top-left-radius: 0;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
     padding: ${({ theme }) => theme.sizeIn(-10)} ${({ theme }) => theme.sizeIn(-4)};
     width: fit-content;
   }
   > span {
-    color: ${({ theme }) => theme.colorPrimary};
+    color: ${({ theme }) => theme.colorBg};
     font-size: ${({ theme }) => theme.sizeIn(-10)};
+    transform: rotateY(-180deg);
   }
 `;
