1d9e2de19ca19088539c76d985ec2044060dfc04
le.zheng <le.zheng@ef.com>
2018-04-09 15:43:40 +0800

    fix a type issue

--- modules/icons/components/Icon/component.tsx
+++ modules/icons/components/Icon/component.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export interface Props {
   src: any;
   className?: string;
-  onClick?: (evt: React.KeyboardEvent<HTMLDivElement>) => void;
+  onClick?: () => void;
 }
 
 class Component extends React.Component<Props> {
